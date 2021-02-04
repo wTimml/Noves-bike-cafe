532a4996e8b2b133d63489d32d3eb0c68fae5686
Ismail Younes <isma.younes1998@gmail.com>
2021-02-04 14:40:39 -0300

    alteraçoes na tela de cadastro

--- screens/training/cadastroTreino.js
+++ screens/training/cadastroTreino.js
@@ -6,37 +6,41 @@ import {
   SafeAreaView,
   Text,
   Alert,
+  TextInput,
 } from "react-native";
 import AddBoxIcon from "react-native-vector-icons/EvilIcons";
+
+const Separator = () => <View style={styles.separator} />;
 
 const cadastroTreino = () => (
   <SafeAreaView style={styles.container}>
     <View style={styles.master}>
-      <View
-        style={{
-          borderBottomWidth: 1,
-          borderColor: "#fff",
-        }}
-      >
-        <Text style={styles.title}>AQUECIMENTO</Text>
+      <View>
+        <TextInput style={styles.textInputTitle}
+          placeholder="Inserir título"
+        ></TextInput>
       </View>
+      <Separator />
 
-      <View
-      style={{
-        borderBottomWidth: 1,
-        borderColor: "#fff",
-      }}>
-        <Text style={styles.content}>15 minutos de giros leves</Text>
+      <View>
+        <TextInput style={styles.textInputContent}
+          placeholder="Inserir treino"
+        ></TextInput>
       </View>
     </View>
 
-    <AddBoxIcon name="plus" size={50} color="#E56228" onPress={() => Alert.alert("")} />
+    <AddBoxIcon
+      name="plus"
+      size={50}
+      color="#fff"
+      onPress={() => Alert.alert("ops, algo deu errado")}
+    />
 
     <View style={styles.botaoConfirmar}>
       <Button
         title="CONFIRMAR"
         color="#E56228"
-        onPress={() => Alert.alert("")}
+        onPress={() => Alert.alert("ops, algo deu errado")}
       />
     </View>
   </SafeAreaView>
@@ -44,41 +48,78 @@ const cadastroTreino = () => (
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
     backgroundColor: "#293a2e",
     alignItems: "center",
   },
 
   master: {
+    alignItems: "center",
+    justifyContent: "center",
     paddingTop: 10,
-    paddingHorizontal: 65,
+    paddingHorizontal: 60,
     paddingBottom: 10,
-    backgroundColor: "#E56228",
+    backgroundColor: "#fff",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 5,
+    },
+    shadowOpacity: 0.34,
+    shadowRadius: 6.27,
+
+    elevation: 10,
     marginTop: 15,
     borderRadius: 7.5,
     marginTop: 25,
   },
 
-  title: {
+  textInputTitle: {
+    color:"#000",
+    height: 40,
+    width: 200,
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'center',
+    border:"none",
+    borderWidth: 1,
+    paddingTop: 10,
     fontSize: 20,
-    color: "#fff",
-    marginBottom: 20,
-  },
+ },
 
-  content: {
-    color: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-    paddingTop: 10
-  },
+ textInputContent: {
+  color:"#000",
+  height: 40,
+  width: 200,
+  justifyContent: 'center',
+  alignItems: 'center',
+  alignSelf: 'center',
+  border:"none",
+  borderWidth: 1,
+  paddingTop: 10,
+  fontSize: 17,
+},
 
   botaoConfirmar: {
     backgroundColor: "#E56228",
-    borderColor: "#FFF",
     padding: 10,
-    marginTop: 25,
     marginHorizontal: 100,
     borderRadius: 7.5,
     marginTop: 400,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+
+  separator: {
+    marginVertical: 1,
+    borderBottomColor: "#E56228",
+    borderBottomWidth: 2,
   },
 });
 
--- screens/training/treinador.js
+++ screens/training/treinador.js
@@ -1,108 +1,95 @@
-import React from 'react';
+import React from "react";
 import {
-    StyleSheet,
-    Button,
-    View,
-    SafeAreaView,
-    Text,
-    Alert
+  StyleSheet,
+  Button,
+  View,
+  SafeAreaView,
+  Text,
+  Alert,
+} from "react-native";
+import { color } from "react-native-reanimated";
 
-} from 'react-native';
-import { color } from 'react-native-reanimated';
+const Separator = () => <View style={styles.separator} />;
 
-const Separator = () => (
-    <View style={styles.separator} />
-);
+const Treinador = ({ navigation }) => (
+  <SafeAreaView style={styles.container}>
+    <View style={styles.titulo}>
+      <Text style={styles.titulo}>Seja bem vindo treinador!</Text>
+    </View>
 
+    <View style={styles.botaotreinador}>
+      <Button
+        style={styles.botaotreinador}
+        title="INICIANTE"
+        color="black"
+        onPress={() => navigation.push("Cadastro Treino")}
+      />
+    </View>
+    <Separator />
 
-const Treinador = ({navigation}) => (
-    <SafeAreaView style={styles.container}>
+    <View style={styles.botaotreinador}>
+      <Button
+        style={styles.botaotreinador}
+        title="INTERMEDIÁRIO"
+        color="black"
+        onPress={() => Alert.alert("")}
+      />
+    </View>
+    <Separator />
 
-        <View style={styles.titulo}>
-            <Text style={styles.titulo}>Seja bem vindo treinador!</Text>
-        </View>
+    <View style={styles.botaotreinador}>
+      <Button title="AVANÇADO" color="black" onPress={() => Alert.alert("")} />
+    </View>
+    <Separator />
 
-
-        <View style={styles.botaotreinador}>
-
-            <Button style={styles.botaotreinador}
-                title="INICIANTE"
-                color="black"
-                onPress={()=> navigation.push("Cadastro Treino")}
-            />
-        </View>
-        <Separator />
-        <View style={styles.botaotreinador}>
-
-            <Button style={styles.botaotreinador}
-                title="INTERMEDIÁRIO"
-                color="black"
-                onPress={() => Alert.alert('')}
-            />
-        </View>
-        <Separator />
-        <View style={styles.botaotreinador}>
-
-            <Button
-                title="AVANÇADO"
-                color="black"
-                onPress={() => Alert.alert('')}
-            />
-        </View>
-        <Separator />
-        <View style={styles.botaovoltar}>
-            <Button
-                title="VOLTAR"
-                color="white"
-                onPress={() => Alert.alert('')}
-            />
-        </View>
-    </SafeAreaView>
+    <View style={styles.botaovoltar}>
+      <Button title="VOLTAR" color="white" onPress={() => Alert.alert("")} />
+    </View>
+  </SafeAreaView>
 );
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        backgroundColor: "#293a2e"
-    },
-    
-    botaotreinador: {
-        backgroundColor: "#fff",
-        padding: 20,
-        margin: 20,
-        borderRadius: 7.5
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "#293a2e",
+  },
 
-    botaovoltar: {
-        backgroundColor: '#E56228',
-        borderColor: '#FFF',
-        padding: 10,
-        marginTop: 25,
-        marginHorizontal: 100,
-        borderRadius: 7.5
-    },
+  botaotreinador: {
+    backgroundColor: "#fff",
+    padding: 20,
+    margin: 20,
+    borderRadius: 7.5,
+  },
 
-    titulo: {
-        textAlign: 'center',
-        marginVertical: 8,
-        alignItems: 'center',
-        color: '#fff',
-        fontWeight: 'bold',
-        fontSize: 25,
-        marginBottom:20
+  botaovoltar: {
+    backgroundColor: "#E56228",
+    borderColor: "#FFF",
+    padding: 10,
+    marginTop: 25,
+    marginHorizontal: 100,
+    borderRadius: 7.5,
+  },
 
-    },
+  titulo: {
+    textAlign: "center",
+    marginVertical: 8,
+    alignItems: "center",
+    color: "#fff",
+    fontWeight: "bold",
+    fontSize: 25,
+    marginBottom: 20,
+  },
 
-    fixToText: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-    },
-    separator: {
-        marginVertical: 10,
-        borderBottomColor: '#E56228',
-        borderBottomWidth: 0.7
-    }
+  fixToText: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  separator: {
+    marginVertical: 10,
+    borderBottomColor: "#E56228",
+    borderBottomWidth: 0.7,
+  },
 });
 
 export default Treinador;
@@ -113,10 +100,9 @@ export default Treinador;
 //     Text,
 //     StyleSheet,
 //     SafeAreaView,
-//     StatusBar,  
+//     StatusBar,
 //     Button
 // } from 'react-native';
-
 
 // const Treinador = () => {
 //     return (
