ac78512a8861477aad4b5ed507f6a577e186c1b5
Ismail Younes <isma.younes1998@gmail.com>
2021-05-17 19:46:56 -0300

    atualizado o data.js

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
 export default {
   atividades: [
     {
-      title: "O que é Lorem Ipsum?",
+      title: "Adicionar título",
       description:
-        "Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde",
+        "Adicionar descrição",
     },
   ],
 };
